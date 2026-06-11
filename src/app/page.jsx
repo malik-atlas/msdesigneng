c7eee6ce99ff0d97a55cd697d199042eec35e247
malik-atlas <malikux@proton.me>
2026-06-11 19:32:25 +0000

    text change

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -12,7 +12,7 @@ export default function Home() {
       <Container className="mt-24 sm:mt-32">
         <FadeIn className="max-w-3xl">
           <h1 className="font-display text-5xl font-medium tracking-tight text-neutral-950 [text-wrap:balance] sm:text-7xl">
-            Hi, there! I'm Malik. 
+            Hi, there! I'm Malik Smith. 
           </h1>
           <p className="mt-6 text-xl text-neutral-600">
           I run Atlas, a design engineering company that prides it on working at the intersection of design and technology. Creative clarity in design is about putting the human experience first and finding technology solutions that bridge the divide.
